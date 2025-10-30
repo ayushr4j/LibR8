76d49b7c78ed381006d41e7ae0d1aa7cbdf7888a
Ayush Raj <ayushr4j@gmail.com>
2025-10-30 22:47:10 +0530

    reference

--- include/core/type/reference.hpp
+++ include/core/type/reference.hpp
@@ -6,10 +6,38 @@ namespace ar4j
     //no namespace for ease of using basic types. basic types are most commonly used part of frameworks
     
     
-    
+    class A{
+        int a;
+        float b;
+        double c;
+        struct {
+            int a; 
+            float b;
+            double c;
+        } d;
+    };
+
+    template<typename t>
     class Reference{
+        static t dummy;
+        template<typename R>
+        Reference<R> operator->();
 
     };
+
+    template<typename T>
+    template<typename t >
+    Reference<t> Reference<T>::operator->(){
+        if constexpr (std::is_pointer_v<R>)
+            return dummy;  // R is pointer type
+        else
+            return Reference<t>{};
+    }
+
+    Reference<A> ref;
+    
+
+    
 
 
 
---
+++ src/core/type/reference.cpp
@@ -0,0 +1,3 @@
+
+#include "core/type/reference.hpp"
+
